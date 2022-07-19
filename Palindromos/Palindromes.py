11861bdc56bfd11f204fcc1620a4491b2ee78eeb
Tomas Prott <44026752+Tomasprb@users.noreply.github.com>
2022-07-19 18:59:23 -0300

    palindromo bugfix

--- Palindromos/Palindromes.py
+++ Palindromos/Palindromes.py
@@ -9,6 +9,7 @@ NormalCounter=0
 WordCounter=0
 
 palabra=input("Ingrese una palabra: ")
+WordCounter+=1
 while palabra != 'aloha' and palabra != 'bye':
     WordCounter+=1
     if palindrome(palabra)==True:
@@ -16,6 +17,7 @@ while palabra != 'aloha' and palabra != 'bye':
     else:
         NormalCounter+=1
     palabra=input("Ingrese una palabra: ")
+    WordCounter+=1
 print("la cantidad de palabras fue: ", WordCounter)
 print("la cantidad de palindromos fue: ", PalindromeCounter)
 print("la cantidad de palabras normales fue: ", NormalCounter)
