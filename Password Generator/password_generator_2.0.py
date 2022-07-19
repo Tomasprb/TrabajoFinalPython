92131bdc718d8179b0dbdcd4701773742b150621
Tomas Prott <44026752+Tomasprb@users.noreply.github.com>
2022-07-18 21:26:11 -0300

    creador de contraseñas 2.0

---
+++ Password Generator/password_generator_2.0.py
@@ -0,0 +1,43 @@
+from email import charset
+import random
+from operator import length_hint
+from random import randint
+from typing import List
+
+
+leng = input("Ingrese longitud: ")
+if leng=='':
+    leng=16
+up = input("Desea mayusculas? (y/n): ")
+if up=='':
+    up='y'
+low = input("Desea minusculas? (y/n): ")
+if low=='':
+    low='y'
+num = input("Desea numeros? (y/n): ")
+if num=='':
+    num='y'
+sim = input("Desea caracteres especiales? (y/n): ")
+if sim=='':
+    sim='n'
+
+upList = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+lowList = upList.lower()
+numList = "0123456789"
+simList = "+-%$&#"
+
+def CarAleatorio(str):
+    return str[random.randint(0 , len(str)-1)]
+Password=""
+charSet=""
+if up=='y':
+    charSet+=upList
+if low=='y':
+    charSet+=lowList
+if num=='y':
+    charSet+=numList
+if sim=='y':
+    charSet+=simList
+for i in range (int(leng)):
+    Password+=CarAleatorio(charSet)
+print("Su contraseña es: " + Password)
